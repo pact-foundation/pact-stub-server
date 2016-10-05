b3213fabda4329a1df2ea085847fe1272d1aeb85
Ronald Holshausen <rholshausen@dius.com.au>
2016-10-05 20:03:35 +1100

    correct the release osx script

--- release-osx.sh
+++ release-osx.sh
@@ -2,5 +2,5 @@
 cargo clean
 cargo build --release
 cargo build --release --target x86_64-apple-ios
-gzip -c target/release/pact_verifier_cli > target/release/pact_verifier_cli-osx-x86_64-$1.gz
-gzip -c target/x86_64-apple-ios/release/pact_verifier_cli > target/x86_64-apple-ios/release/pact_verifier_cli-ios-x86_64-$1.gz
+gzip -c target/release/pact-stub-server > target/release/pact-stub-server-osx-x86_64-$1.gz
+gzip -c target/x86_64-apple-ios/release/pact-stub-server > target/x86_64-apple-ios/release/pact-stub-server-ios-x86_64-$1.gz
