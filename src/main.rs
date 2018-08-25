1f2855e95e540295c67dac908db64d87f302f7f8
Ronald Holshausen <uglyog@gmail.com>
2018-08-25 20:58:32 +1000

    feat: implemented support for fetching pacts using HTTPS #13

--- src/main.rs
+++ src/main.rs
@@ -61,8 +61,10 @@
 #[cfg(test)]
 #[macro_use(expect)]
 extern crate expectest;
-extern crate hyper;
 extern crate http;
+extern crate hyper;
+extern crate hyper_tls;
+extern crate tokio;
 extern crate itertools;
 #[macro_use] extern crate log;
 #[macro_use] extern crate maplit;
@@ -74,8 +76,15 @@ extern crate quickcheck;
 extern crate rand;
 extern crate serde_json;
 extern crate simplelog;
+extern crate base64;
 
 use clap::{App, AppSettings, Arg, ArgMatches, ErrorKind};
+use http::StatusCode;
+use hyper::{Body, Request as HyperRequest, Response as HyperResponse, Server};
+use hyper::Client;
+use hyper_tls::HttpsConnector;
+use hyper::rt::{Future, Stream};
+use hyper::service::service_fn_ok;
 use itertools::Itertools;
 use log::LogLevelFilter;
 use pact_matching::*;
@@ -87,10 +96,8 @@ use std::io;
 use std::path::Path;
 use std::str::FromStr;
 use std::sync::Arc;
-use hyper::{Body, Request as HyperRequest, Response as HyperResponse, Server};
-use hyper::service::service_fn_ok;
-use hyper::rt::{self, Future};
-use http::StatusCode;
+use tokio::runtime::Runtime;
+use base64::encode;
 
 mod pact_support;
 
@@ -118,7 +125,7 @@ pub enum PactSource {
     /// Load all the pacts from a Directory
     Dir(String),
     /// Load the pact from a URL
-    URL(String)
+    URL(String, Option<String>)
 }
 
 fn pact_source(matches: &ArgMatches) -> Vec<PactSource> {
@@ -132,7 +139,9 @@ fn pact_source(matches: &ArgMatches) -> Vec<PactSource> {
         None => ()
     };
     match matches.values_of("url") {
-        Some(values) => sources.extend(values.map(|v| PactSource::URL(s!(v))).collect::<Vec<PactSource>>()),
+        Some(values) => sources.extend(values.map(|v| {
+            PactSource::URL(s!(v), matches.value_of("user").map(|u| u.to_string()))
+        }).collect::<Vec<PactSource>>()),
         None => ()
     };
     sources
@@ -152,24 +161,44 @@ fn walkdir(dir: &Path) -> io::Result<Vec<io::Result<Pact>>> {
     Ok(pacts)
 }
 
-fn pact_from_url(url: &String) -> Result<Pact, String> {
-//    let client = Client::new();
-//    match client.get(url).send() {
-//        Ok(mut res) => if res.status.is_success() {
-//                let pact_json = serde_json::from_reader(&mut res);
-//                match pact_json {
-//                    Ok(ref json) => Ok(Pact::from_json(url, json)),
-//                    Err(err) => Err(format!("Failed to parse Pact JSON - {}", err))
-//                }
-//            } else {
-//                Err(format!("Request failed with status - {}", res.status))
-//            },
-//        Err(err) => Err(format!("Request failed - {}", err))
-//    }
-    unimplemented!()
+fn pact_from_url(url: String, user: &Option<String>, runtime: &mut Runtime) -> Result<Pact, String> {
+    match url.parse::<hyper::Uri>() {
+        Ok(uri) => {
+            let https = HttpsConnector::new(4).unwrap();
+            let mut req = HyperRequest::builder();
+            req.uri(uri).method("GET");
+            match user {
+                Some(ref u) => { req.header("Authorization", format!("Basic {}", encode(u))); },
+                None => ()
+            }
+            debug!("Executing Request to fetch pact from URL: {:?}", req);
+            let client = Client::builder()
+                .build::<_, hyper::Body>(https);
+            let future = client
+                .request(req.body(Body::empty()).unwrap())
+                .map_err(|err| format!("Request failed - {}", err))
+                .and_then(|res| {
+                    if res.status().is_success() {
+                        Ok(res)
+                    } else {
+                        Err(format!("Request failed - {}", res.status()))
+                    }
+                })
+                .and_then(|res| res.into_body().concat2().map_err(|err| format!("Failed to read the request body - {}", err)))
+                .and_then(move |body| {
+                    let pact_json = serde_json::from_slice(&body)
+                        .map_err(|err| format!("Failed to parse Pact JSON - {}", err))?;
+                    let pact = Pact::from_json(&url, &pact_json);
+                    debug!("Fetched Pact: {:?}", pact);
+                    Ok(pact)
+                });
+            runtime.block_on(future)
+        },
+        Err(err) => Err(format!("Request failed - {}", err))
+    }
 }
 
-fn load_pacts(sources: Vec<PactSource>) -> Vec<Result<Pact, String>> {
+fn load_pacts(sources: Vec<PactSource>, runtime: &mut Runtime) -> Vec<Result<Pact, String>> {
     sources.iter().flat_map(|s| {
         match s {
             &PactSource::File(ref file) => vec![Pact::read_pact(Path::new(&file))
@@ -183,7 +212,7 @@ fn load_pacts(sources: Vec<PactSource>) -> Vec<Result<Pact, String>> {
                     }).collect(),
                 Err(err) => vec![Err(format!("Could not load pacts from directory '{}' - {}", dir, err))]
             },
-            &PactSource::URL(ref url) => vec![pact_from_url(url)
+            &PactSource::URL(ref url, ref user) => vec![pact_from_url(url.clone(), user, runtime)
                 .map_err(|err| format!("Failed to load pact '{}' - {}", url, err))]
         }
     })
@@ -268,7 +297,7 @@ impl ServerHandler {
     }
 }
 
-fn start_server(port: u16, sources: Vec<Pact>, auto_cors: bool) -> Result<(), i32> {
+fn start_server(port: u16, sources: Vec<Pact>, auto_cors: bool, runtime: &mut Runtime) -> Result<(), i32> {
     let addr = ([0, 0, 0, 0], port).into();
     match Server::try_bind(&addr) {
         Ok(builder) => {
@@ -278,8 +307,11 @@ fn start_server(port: u16, sources: Vec<Pact>, auto_cors: bool) -> Result<(), i3
                     service_fn_ok(move |req| service_handler.handle(req))
                 });
             info!("Server started on port {}", server.local_addr().port());
-            rt::run(server.map_err(|err| error!("could not start server: {}", err)));
-            Ok(())
+            runtime.block_on(server.map_err(|err| error!("could not start server: {}", err)))
+                .map_err(|_| {
+                    format!("error occurred scheduling server future on Tokio runtime");
+                    2
+                })
         },
         Err(err) => {
             error!("could not start server: {}", err);
@@ -336,6 +368,13 @@ fn handle_command_args() -> Result<(), i32> {
             .number_of_values(1)
             .empty_values(false)
             .help("URL of pact file to verify (can be repeated)"))
+        .arg(Arg::with_name("user")
+            .long("user")
+            .takes_value(true)
+            .use_delimiter(false)
+            .number_of_values(1)
+            .empty_values(false)
+            .help("User and password to use when fetching pacts from URLS in user:password form"))
         .arg(Arg::with_name("port")
             .short("p")
             .long("port")
@@ -356,17 +395,20 @@ fn handle_command_args() -> Result<(), i32> {
             let level = matches.value_of("loglevel").unwrap_or("info");
             setup_logger(level);
             let sources = pact_source(matches);
-            let pacts = load_pacts(sources);
+
+            let mut tokio_runtime = Runtime::new().unwrap();
+            let pacts = load_pacts(sources, &mut tokio_runtime);
             if pacts.iter().any(|p| p.is_err()) {
                 error!("There were errors loading the pact files.");
                 for error in pacts.iter().filter(|p| p.is_err()).cloned().map(|e| e.unwrap_err()) {
                     error!("  - {}", error);
                 }
+                tokio_runtime.shutdown_now();
                 Err(3)
             } else {
                 let port = matches.value_of("port").unwrap_or("0").parse::<u16>().unwrap();
                 start_server(port, pacts.iter().cloned().map(|p| p.unwrap()).collect(),
-                             matches.is_present("cors"))
+                             matches.is_present("cors"), &mut tokio_runtime)
             }
         },
         Err(ref err) => {
