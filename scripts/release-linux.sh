c2be7b1b4b6877dca01c3e38baf130e25733d4c4
Ronald Holshausen <ronald.holshausen@smartbear.com>
2022-10-24 15:19:46 +1100

    chore: Add aarch64 (ARM64) binaries to the release build #50

--- scripts/release-linux.sh
+++ scripts/release-linux.sh
@@ -8,3 +8,10 @@ docker build -f Dockerfile.linux-build -t linux-build .
 mkdir -p target/artifacts
 docker run -t --rm --user "$(id -u)":"$(id -g)" -v "$(pwd):/workspace" -w /workspace linux-build -c 'cargo build --release'
 gzip -c target/release/pact-stub-server > target/artifacts/pact-stub-server-linux-x86_64.gz
+openssl dgst -sha256 -r target/artifacts/pact-stub-server-linux-x86_64.gz > target/artifacts/pact-stub-server-linux-x86_64.gz.sha256
+
+echo -- Build the aarch64 release artifacts --
+cargo install cross
+cross build --target aarch64-unknown-linux-gnu --release
+gzip -c target/aarch64-unknown-linux-gnu/release/pact-stub-server > target/artifacts/pact-stub-server-linux-aarch64.gz
+openssl dgst -sha256 -r target/artifacts/pact-stub-server-linux-aarch64.gz > target/artifacts/pact-stub-server-linux-aarch64.gz.sha256
--- scripts/release-osx.sh
+++ scripts/release-osx.sh
@@ -5,3 +5,11 @@ set -e
 mkdir -p target/artifacts
 cargo build --release
 gzip -c target/release/pact-stub-server > target/artifacts/pact-stub-server-osx-x86_64.gz
+openssl dgst -sha256 -r target/artifacts/pact-stub-server-osx-x86_64.gz > target/artifacts/pact-stub-server-osx-x86_64.gz.sha256
+
+# M1
+export SDKROOT=$(xcrun -sdk macosx11.1 --show-sdk-path)
+export MACOSX_DEPLOYMENT_TARGET=$(xcrun -sdk macosx11.1 --show-sdk-platform-version)
+cargo build --target aarch64-apple-darwin --release
+gzip -c target/aarch64-apple-darwin/release/pact_verifier_cli > target/artifacts/pact-stub-server-osx-aarch64.gz
+openssl dgst -sha256 -r target/artifacts/pact-stub-server-osx-aarch64.gz > target/artifacts/pact-stub-server-osx-aarch64.gz.sha256
