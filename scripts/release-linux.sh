82beb3ebfa496544e6918b9ca0327f7159420d3c
Yousaf Nabi <you@saf.dev>
2024-02-08 22:48:21 +0000

    chore: fixup path to musl artifact

--- scripts/release-linux.sh
+++ scripts/release-linux.sh
@@ -22,11 +22,11 @@ sudo apt install musl-tools
 rustup target add x86_64-unknown-linux-musl
 cargo clean
 cargo build --release --target=x86_64-unknown-linux-musl
-gzip -c target/x86_64-unknown-linux-musl/release/pact_mock_server_cli > release_artifacts/pact_mock_server_cli-linux-x86_64-musl.gz
-openssl dgst -sha256 -r release_artifacts/pact_mock_server_cli-linux-x86_64-musl.gz > release_artifacts/pact_mock_server_cli-linux-x86_64-musl.gz.sha256
+gzip -c target/x86_64-unknown-linux-musl/release/pact-stub-server > release_artifacts/pact-stub-server-linux-x86_64-musl.gz
+openssl dgst -sha256 -r release_artifacts/pact-stub-server-linux-x86_64-musl.gz > release_artifacts/pact-stub-server-linux-x86_64-musl.gz.sha256
 
 echo -- Build the musl aarch64 release artifacts --
 cargo clean
 cross build --release --target=aarch64-unknown-linux-musl
-gzip -c target/aarch64-unknown-linux-musl/release/pact_mock_server_cli > release_artifacts/pact_mock_server_cli-linux-aarch64-musl.gz
-openssl dgst -sha256 -r release_artifacts/pact_mock_server_cli-linux-aarch64-musl.gz > release_artifacts/pact_mock_server_cli-linux-aarch64-musl.gz.sha256
+gzip -c target/aarch64-unknown-linux-musl/release/pact-stub-server > release_artifacts/pact-stub-server-linux-aarch64-musl.gz
+openssl dgst -sha256 -r release_artifacts/pact-stub-server-linux-aarch64-musl.gz > release_artifacts/pact-stub-server-linux-aarch64-musl.gz.sha256
