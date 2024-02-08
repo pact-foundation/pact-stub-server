b5f8418f09cafabcdfffafeed284e4a747e748d7
Yousaf Nabi <you@saf.dev>
2024-02-08 21:54:44 +0000

    feat: build for musl

--- scripts/release-win.sh
+++ scripts/release-win.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-mkdir -p target/artifacts
+mkdir -p release_artifacts
 cargo build --release
-gzip -c target/release/pact-stub-server.exe > target/artifacts/pact-stub-server-windows-x86_64.exe.gz
+gzip -c target/release/pact-stub-server.exe > release_artifacts/pact-stub-server-windows-x86_64.exe.gz
